c6d2f0e2d409d2a1996cdd2536982e87bed1f6ef
Sam Lewis <samuelewis@gmail.com>
2016-03-28 19:32:33 -0400

    Added status light for when Wifi configuration is required

--- TempLogger.ino
+++ TempLogger.ino
@@ -5,6 +5,7 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager v: 0.10.0
 
+#define LED 4
 #define BUTTON 13
 #define DHTPIN 2
 #define DHTTYPE DHT22
@@ -18,6 +19,7 @@ WiFiManager wifiManager;
 
 void setup() {
 
+  pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
   
   EEPROM.begin(512);
@@ -26,7 +28,10 @@ void setup() {
   
   WiFiManagerParameter param("Thingspeak", "thingspeak key", thingspeakKey, 40);
   wifiManager.addParameter(&param);
+
+  digitalWrite(LED, HIGH);
   wifiManager.autoConnect("TempLogger");
+  digitalWrite(LED, LOW);
   
   strcpy(thingspeakKey, param.getValue());
   
@@ -41,7 +46,9 @@ void loop() {
   if (digitalRead(BUTTON) == LOW) {
     // startConfigPortal causes issues with custom parameter
     wifiManager.resetSettings();
+    digitalWrite(LED, HIGH);
     wifiManager.autoConnect("TempLogger");
+    digitalWrite(LED, LOW);
   }
   float h = dht.readHumidity();
   float t = dht.readTemperature();
